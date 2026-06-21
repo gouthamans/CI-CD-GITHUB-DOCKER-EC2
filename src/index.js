ca65bce540c78da6f37efdbdb6e75dd8536c0300
gouthamans <sgoutham@gmail.com>
2026-06-21 21:50:47 +0530

    testing commit

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Server Running');
+    res.send('Server Running successfully');
 });
 
 const PORT = 3000;
